265794c9e78a9f765b160fff396a535b10e304b2
Anna Shiba <102438674+AnnaShiba@users.noreply.github.com>
2023-06-26 17:30:55 -0400

    Add IntelliJ project to the repository

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, enter which dice do you want to use: ");
+        int dice = input.nextInt();
+        System.out.println("Please, enter how much dices you want to roll: ");
+        int roll = input.nextInt();
+        Random rand = new Random();
+        int total = 0;
+        for(int i = 0; i <= roll; i++) {
+            int result = (rand.nextInt(dice) + 1);
+            total = result + total;
+            System.out.println("Your roll is: " + result);
+        }
+        System.out.println("The result is: " + total);
+    }
+}
